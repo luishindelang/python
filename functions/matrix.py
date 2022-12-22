22fd905d8ff10602bd13e55e31de1ef2fd77a393
luishindelang <luis.hindelang@gmail.com>
2022-12-22 18:00:24 +0100

    update

--- functions/matrix.py
+++ functions/matrix.py
@@ -1,25 +1,23 @@
 def multiplyMatrix(m1, m2):
-    result = list()
-
     m1x = len(m1[0])
     m1y = len(m1)
     m2x = len(m2[0])
-
-    x2 = 0
-    while x2 < m2x:
-        r = list()
-        y1 = 0
-        while y1 < m1y:
+    my = list()
+    y1 = 0
+    while y1 < m1y:
+        mx = list()
+        x2 = 0
+        while x2 < m2x:
             x1 = 0
             z = 0
             while x1 < m1x:
                 z += m1[y1][x1] * m2[x1][x2]
                 x1 += 1
-            r.append(z)
-            y1 += 1
-        result.append(r)
-        x2 += 1
-    return result
+            mx.append(z)
+            x2 += 1
+        my.append(mx)
+        y1 += 1
+    return my
 
 
 def uniformMatrix(m):
@@ -35,3 +33,58 @@ def uniformMatrix(m):
                 x.append(0)
                 counter += 1
     return m
+
+
+def listToMatrix(l):
+    my = list()
+    lenth = len(l)
+    y = 0
+
+    while y < lenth:
+        x = y
+        mx = list()
+        while x < lenth:
+            mx.append(l[x])
+            x += 1
+        my.append(mx)
+        y += 1
+    return my
+
+
+def nullMatrix(lenth):
+    my = list()
+    y = 0
+    while y < lenth:
+        x = 0
+        mx = list()
+        while x < lenth:
+            mx.append(0)
+            x += 1
+        my.append(mx)
+        y += 1
+    return my
+
+
+def einheitsMatrix(lenth):
+    m = nullMatrix(lenth)
+    xy = 0
+    while xy < lenth:
+        m[xy][xy] = 1
+        xy += 1
+
+    return m
+
+
+def transponieren(m):
+    lenth = len(m)
+    my = list()
+    x = 0
+    while x < lenth:
+        y = 0
+        mx = list()
+        while y < lenth:
+            mx.append(m[y][x])
+            y += 1
+        my.append(mx)
+        x += 1
+    return my
--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 import numpy
 import json
 from functions.getData import readData, writeData
+from functions.matrix import listToMatrix, uniformMatrix, multiplyMatrix, nullMatrix, einheitsMatrix, transponieren
+
+
+def showMatrix(m):
+    for x in m:
+        print(x)
 
 # with open('./words/nomen.txt', 'r', encoding='utf-8') as file:
 #     x = file.read()
@@ -20,8 +26,43 @@ from functions.getData import readData, writeData
 
 # print(newList)
 
-x = 10
+# x = ['hallo']
 
-x *= 2
+# if type(x) == list:
+#     print(x)
 
-print(x)
+
+l1 = [1, 2, 3, 4]
+m1 = uniformMatrix(listToMatrix(l1))
+m2 = [
+    [0, 0, 0, 1],
+    [0, 0, 1, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1]
+]
+m3 = [
+    [1, 0, 0, 0],
+    [1],
+    [1],
+    [1]
+]
+m4 = [
+    [0, 0, 0, 1],
+    [0, 0, 1],
+    [0, 1],
+    [1]
+]
+showMatrix(uniformMatrix(m3))
+
+print('-->')
+
+showMatrix(multiplyMatrix(uniformMatrix(m3), einheitsMatrix(4)))
+# showMatrix(multiplyMatrix(uniformMatrix(listToMatrix(l1)),
+#            uniformMatrix(listToMatrix(l1))))
+# showMatrix(transponieren(uniformMatrix(m3)))
+
+
+# m1 = [[3, 2, 1], [1, 0, 2]]
+# m2 = [[1, 2], [0, 1], [4, 0],]
+
+# print(multiplyMatrix(m1, m2))
